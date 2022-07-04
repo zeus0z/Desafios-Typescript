1b6135ad0a9a88535c162979548790c974716684
Zeus Amorim <amorimzeus@gmail.com>
2022-07-03 23:05:49 -0300

    Resolvido o primeiro desafio

---
+++ desafio1.ts
@@ -0,0 +1,21 @@
+/* Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+
+employee.code = 10;
+employee.name = "John";
+
+*/
+
+
+
+// Resposta: Podemos criar o objeto e deixar o Typscript inferir os tipos de dados
+
+let employee = {
+    code: 10,
+    name: 10,
+
+};
+
+let class:string= 'deablo'
+;
